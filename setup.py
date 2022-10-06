2a2baef2908e6adc8df7b3a3d942d33495772780
CaiZhou2 <1849834742@qq.com>
2022-10-06 18:41:42 +1100

    c++17 support for pip build

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
+import platform
 from glob import glob
 from setuptools import setup
 from pybind11.setup_helpers import Pybind11Extension, ParallelCompile, naive_recompile
 
-__version__ = "0.2.0"
+__version__ = "0.2.2"
 
 # `N` is to set the bumer of threads
 # `naive_recompile` makes it recompile only if the source file changes. It does not check header files!
@@ -25,9 +26,19 @@ include_dirs = ["./c_src/include"]
 # runtime_library_dirs = [os.path.abspath("LINK_DIR")]
 # libraries = ["abc"]
 
+# Extra parameters to the compiler
+cpp_extra_compile_args = []
+if platform.system() == "Darwin":
+    # cpp_extra_compile_args.append('-stdlib=libc++')
+    cpp_extra_compile_args.append('-mmacosx-version-min=10.15')
+    # cpp_extra_link_args.append('-stdlib=libc++')
+    # cpp_extra_link_args.append('-mmacosx-version-min=10.7')
+
+# Set ext modules.
 ext_modules = [
     Pybind11Extension(
         name="PyBTLS.c_module._core", # depends on the structure of your package
+        extra_compile_args = cpp_extra_compile_args,
         sources=source_files,
         # Example: passing in the version to the compiled code
         # define_macros=[("VERSION_INFO", __version__)],
@@ -35,12 +46,13 @@ ext_modules = [
         # library_dirs=library_dirs,
         # runtime_library_dirs=runtime_library_dirs,
         # libraries=libraries,
-        cxx_std=14,
+        cxx_std=17,
         language="c++"
     ),
 ]
 
 
+# Finish setup()
 setup(
     version = __version__,
     ext_modules=ext_modules,
