9f6d3d33292221828e8021f14af6aed8dbad463e
Colin Caprani <colin.caprani@monash.edu>
2024-06-26 20:04:29 +1000

    Ensure library on path

--- py/pybtls/__init__.py
+++ py/pybtls/__init__.py
@@ -4,6 +4,11 @@ Bridge Traffic Load Simulation with Python.
 
 __version__ = "0.3.7"  # Should also manually update in pyproject.toml
 
+# Add the directory containing the shared library to sys.path
+lib_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "lib"))
+if lib_path not in sys.path:
+    sys.path.insert(0, lib_path)
+
 import pybtls.lib as lib
 import pybtls.bridge as bridge
 import pybtls.traffic as traffic
@@ -11,6 +16,7 @@ import pybtls.output as output
 import pybtls.simulation as simulation
 import pybtls.garage as garage
 import pybtls.utils as utils
+
 # import pybtls.post_processing as post_processing
 
 from .lib import (
