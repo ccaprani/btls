f8b219cd2fc6bad4f508d7a09ec84f44dd068f5e
CaiZhou2 <1849834742@qq.com>
2024-08-19 12:39:30 +1000

    Workflow docs build fix.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,10 +10,6 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
-
-sys.path.insert(0, os.path.abspath("../../py/"))
 from pybtls import __version__ as ver
 
 # -- Project information -----------------------------------------------------
