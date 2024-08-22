c93c928e262d93eac1b5f440a3baa067c400e594
CaiZhou2 <1849834742@qq.com>
2024-08-23 00:12:54 +1000

    Restore the comment in config.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,6 +4,13 @@
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+# sys.path.insert(0, os.path.abspath("../../py/"))  # Add the project root to the system path
+
 from pybtls import __version__ as ver
 
 # -- Project information -----------------------------------------------------
